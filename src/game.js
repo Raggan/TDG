9dcf51f44477ac49b3d2f52fd48174e9efae8795
Raggan <Raggan@users.noreply.github.com>
2017-02-06 14:26:24 +0100

    Added Background

--- src/game.js
+++ src/game.js
@@ -5,7 +5,7 @@ var Game = function(game){
 Game.prototype = {
 
 	create: function() {
-
+    game.add.sprite(0,0,"background");
 	},
 
 	update: function() {
--- src/preload.js
+++ src/preload.js
@@ -3,7 +3,7 @@ var Preload = function(game){};
 Preload.prototype = {
 
 	preload: function(){
-
+		this.game.load.image('background', 'assets/background2.jpg');
 	},
 
 	create: function(){
