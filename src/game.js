a0564be19fe4902cf4ad3d9d5fb822310007b071
Raggan <Raggan@users.noreply.github.com>
2017-02-06 15:06:12 +0100

    Added Player, Castles and Basic Movement

--- src/game.js
+++ src/game.js
@@ -5,10 +5,37 @@ var Game = function(game){
 Game.prototype = {
 
 	create: function() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     game.add.sprite(0,0,"background");
+    game.add.sprite(10,305, "castle2");
+    castle1 = game.add.sprite(670,300, "castle1");
+    //castle1.body.immovable = true;
+    player = game.add.sprite(55, 370, 'dude');
+    game.physics.arcade.enable(player);
+    game.physics.arcade.enable(castle1);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+    castle1.body.moves = false;
+
+
+
 	},
 
 	update: function() {
+
+
+    if (game.physics.arcade.collide(player, castle1)) {
+      //  Stand still
+    player.animations.stop();
+
+    player.frame = 4;
+    }
+    else {
+      //  Move to the right
+    player.body.velocity.x = 150;
+
+    player.animations.play('right');
+  }
 
 	}
 
--- src/preload.js
+++ src/preload.js
@@ -4,6 +4,9 @@ Preload.prototype = {
 
 	preload: function(){
 		this.game.load.image('background', 'assets/background2.jpg');
+		this.game.load.image('castle1', 'assets/castle1_scale.png');
+		this.game.load.image('castle2', 'assets/castle2_scale.png');
+		this.game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 	},
 
 	create: function(){
