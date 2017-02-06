9a4bc0985b79d210aa61cd10910b606094725753
Raggan <Raggan@users.noreply.github.com>
2017-02-06 15:23:48 +0100

    Added Second Player and test for collision

--- src/game.js
+++ src/game.js
@@ -7,16 +7,19 @@ Game.prototype = {
 	create: function() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.add.sprite(0,0,"background");
-    game.add.sprite(10,305, "castle2");
+    castle2 = game.add.sprite(10,305, "castle2");
     castle1 = game.add.sprite(670,300, "castle1");
     //castle1.body.immovable = true;
-    player = game.add.sprite(55, 370, 'dude');
-    game.physics.arcade.enable(player);
+    player1 = game.add.sprite(55, 370, 'dude');
+    player2 = game.add.sprite(715, 370, 'dude');
+    game.physics.arcade.enable(player1);
+    game.physics.arcade.enable(player2);
     game.physics.arcade.enable(castle1);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
-
+    game.physics.arcade.enable(castle2);
+    player1.animations.add('right', [5, 6, 7, 8], 5, true);
+    player2.animations.add('left', [0, 1, 2, 3], 5, true);
     castle1.body.moves = false;
-
+    castle2.body.moves = false;
 
 
 	},
@@ -24,17 +27,23 @@ Game.prototype = {
 	update: function() {
 
 
-    if (game.physics.arcade.collide(player, castle1)) {
-      //  Stand still
-    player.animations.stop();
 
-    player.frame = 4;
-    }
-    else {
+    if (!game.physics.arcade.collide(player1, player2)){
       //  Move to the right
-    player.body.velocity.x = 150;
+    player1.body.velocity.x = 50;
+    player2.body.velocity.x = -50;
 
-    player.animations.play('right');
+    player1.animations.play('right');
+    player2.animations.play('left');
+  }
+
+  else  {
+    //  Stand still
+  player1.animations.stop();
+  player2.animations.stop();
+  player1.frame = 4;
+  player2.frame = 4;
+
   }
 
 	}
