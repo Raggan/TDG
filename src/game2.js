c34c2ec3dd267322cb28fde79f214adda417e9b2
Raggan <Raggan@users.noreply.github.com>
2017-02-08 08:53:11 +0100

    [WIP] Added game2.js for changing mechanics to 1 player
    Resolves: #
    Releases: KW
    Tests: y|n

--- src/game2.js
+++ src/game2.js
@@ -9,18 +9,18 @@ Game.prototype = {
 	  game.add.sprite(0,0,"background");
     castle1 = game.add.sprite(10,250, "castle2");
     castle2 = game.add.sprite(670,240, "castle1");
-    //castle1.body.immovable = true;
+
     player = game.add.sprite(55, 315, 'dude');
-    enemy = game.add.sprite(715, 315, 'enemy');
+
 
 		player.health=100;
-		enemy.health=50;
+
     game.physics.arcade.enable(player);
-    game.physics.arcade.enable(enemy);
+
     game.physics.arcade.enable(castle1);
     game.physics.arcade.enable(castle2);
     player.animations.add('right', [5, 6, 7, 8], 5, true);
-    enemy.animations.add('left', [0, 1, 2, 3], 5, true);
+
     castle1.body.moves = false;
     castle2.body.moves = false;
 
@@ -30,7 +30,7 @@ Game.prototype = {
 	},
 	render: function() {
 	// game.debug.body(player);
-	// game.debug.body(enemy);
+
 	// game.debug.body(castle1);
 	// game.debug.body(castle2);
 },
@@ -47,21 +47,6 @@ Game.prototype = {
 		this.playerMovement(player);
 	}
 
-	if (game.physics.arcade.overlap(enemy, castle1)){
-
-		this.enemyStopMovement(enemy);
-
-	}
-	else {
-		this.enemyMovement(enemy);
-	}
-
-	if (game.physics.arcade.overlap(player, enemy)){
-
-		this.playerStopMovement(player);
-		//this.enemyStopMovement(enemy);
-		//player.kill();
-	}
 
 
 },
@@ -80,17 +65,6 @@ playerStopMovement: function(player) {
 playerMovement: function(player) {
 	player.body.velocity.x = 50;
 	player.animations.play('right');
-},
-
-enemyStopMovement: function(enemy) {
-	enemy.animations.stop();
-	enemy.body.velocity.x = 0;
-	enemy.frame =4;
-},
-
-enemyMovement: function(enemy) {
-	enemy.body.velocity.x = -50;
-	enemy.animations.play('left');
 }
 
 
--- src/preload.js
+++ src/preload.js
@@ -7,6 +7,7 @@ Preload.prototype = {
 		this.game.load.image('castle1', 'assets/castle1_scale.png');
 		this.game.load.image('castle2', 'assets/castle2_scale.png');
 		this.game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+		this.game.load.spritesheet('enemy', 'assets/enemy.png', 32, 48);
 	},
 
 	create: function(){
