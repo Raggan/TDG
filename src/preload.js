35330f021b15c0b8188795f148dffe8a775f1322
Raggan <Raggan@users.noreply.github.com>
2017-02-09 09:10:25 +0100

    [WIP] Changed Spawnbutton to Knight
    Resolves: #
    Releases: KW
    Tests: y|n

--- src/preload.js
+++ src/preload.js
@@ -7,7 +7,7 @@ Preload.prototype = {
 		this.game.load.image('castle1', 'assets/castle1_scale.png');
 		this.game.load.image('castle2', 'assets/castle2_scale.png');
 		this.game.load.spritesheet('minion_weak', 'assets/Knight_Spritesheet_walk.png', 40, 48);
-		this.game.load.image('spawnbutton_minion_weak', 'assets/spawnbutton_minion_weak.png', 40, 40);
+		this.game.load.image('spawnbutton_minion_weak', 'assets/spawnbutton_minion_weak_1.png', 40, 40);
 
 	},
 
